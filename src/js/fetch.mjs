8410e46752765c0c3af1bb6c89dedbdce7d38bec
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-02-21 16:48:41 -0700

    error

--- src/js/fetch.mjs
+++ src/js/fetch.mjs
@@ -5,10 +5,20 @@ const options = {
         "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
     }
 };
-//
+
 export async function fetchData(searchTerm) {
     return fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchTerm}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
-        .then(response => response.json());
-
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            // handle any errors that occurred
+            throw error; // re-throw the error to be caught by the caller
+        });
 }
+
 // console.log(response);
