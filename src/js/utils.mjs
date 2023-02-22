3e903789dc8dbb4343e984466b289696145cc872
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-02-21 21:54:22 -0700

    partials

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,6 +1,6 @@
 export async function renderHeaderFooter() {
-  const headerTemplate = await fetch("../partials/header.html").then(response => response.text());
-  const footerTemplate = await fetch("../partials/footer.html").then(response => response.text());
+  const headerTemplate = await fetch("src/partials/header.html").then(response => response.text());
+  const footerTemplate = await fetch("src/partials/footer.html").then(response => response.text());
   document.querySelector("#main-header").innerHTML = headerTemplate;
   document.querySelector("#main-footer").innerHTML = footerTemplate;
 }
